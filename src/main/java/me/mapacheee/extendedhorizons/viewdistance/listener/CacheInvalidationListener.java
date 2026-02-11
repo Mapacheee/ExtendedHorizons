7993cef139c04c3fba88aa1416be115010137f18
Benjamín Isasmendi C <bisasmendic@gmail.com>
2026-02-11 00:05:24 -0400

    fixing the fix xd

--- src/main/java/me/mapacheee/extendedhorizons/viewdistance/listener/CacheInvalidationListener.java
+++ src/main/java/me/mapacheee/extendedhorizons/viewdistance/listener/CacheInvalidationListener.java
@@ -4,7 +4,6 @@ import com.google.inject.Inject;
 import com.thewinterframework.service.annotation.Service;
 import com.thewinterframework.service.annotation.lifecycle.OnEnable;
 import me.mapacheee.extendedhorizons.viewdistance.service.ChunkLoaderService;
-import me.mapacheee.extendedhorizons.shared.utils.ChunkUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -13,10 +12,7 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
-import org.bukkit.event.world.ChunkUnloadEvent;
 import org.bukkit.plugin.Plugin;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.UUID;
 
 @Service
@@ -24,7 +20,6 @@ public class CacheInvalidationListener implements Listener {
 
     private final ChunkLoaderService chunkLoaderService;
     private final Plugin plugin;
-    private final Set<Long> dirtyChunks = ConcurrentHashMap.newKeySet();
 
     @Inject
     public CacheInvalidationListener(ChunkLoaderService chunkLoaderService, Plugin plugin) {
@@ -37,42 +32,39 @@ public class CacheInvalidationListener implements Listener {
         Bukkit.getPluginManager().registerEvents(this, plugin);
     }
 
-    private void markDirty(UUID worldId, int x, int z) {
-        long key = ChunkUtils.packChunkKey(x, z);
-        dirtyChunks.add(key);
-    }
-
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onBlockBreak(BlockBreakEvent event) {
-        markDirty(event.getBlock().getWorld().getUID(), event.getBlock().getChunk().getX(),
+        chunkLoaderService.invalidateRamCache(
+                event.getBlock().getWorld().getUID(),
+                event.getBlock().getChunk().getX(),
                 event.getBlock().getChunk().getZ());
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onBlockPlace(BlockPlaceEvent event) {
-        markDirty(event.getBlock().getWorld().getUID(), event.getBlock().getChunk().getX(),
+        chunkLoaderService.invalidateRamCache(
+                event.getBlock().getWorld().getUID(),
+                event.getBlock().getChunk().getX(),
                 event.getBlock().getChunk().getZ());
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onBlockExplode(BlockExplodeEvent event) {
-        event.blockList().forEach(
-                block -> markDirty(block.getWorld().getUID(), block.getChunk().getX(), block.getChunk().getZ()));
+        event.blockList().forEach(block -> {
+            UUID worldId = block.getWorld().getUID();
+            int chunkX = block.getChunk().getX();
+            int chunkZ = block.getChunk().getZ();
+            chunkLoaderService.invalidateRamCache(worldId, chunkX, chunkZ);
+        });
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onEntityExplode(EntityExplodeEvent event) {
-        event.blockList().forEach(
-                block -> markDirty(block.getWorld().getUID(), block.getChunk().getX(), block.getChunk().getZ()));
-    }
-
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onChunkUnload(ChunkUnloadEvent event) {
-        long key = ChunkUtils.packChunkKey(event.getChunk().getX(), event.getChunk().getZ());
-        if (dirtyChunks.contains(key)) {
-            chunkLoaderService.invalidateRamCache(event.getWorld().getUID(), event.getChunk().getX(),
-                    event.getChunk().getZ());
-            dirtyChunks.remove(key);
-        }
+        event.blockList().forEach(block -> {
+            UUID worldId = block.getWorld().getUID();
+            int chunkX = block.getChunk().getX();
+            int chunkZ = block.getChunk().getZ();
+            chunkLoaderService.invalidateRamCache(worldId, chunkX, chunkZ);
+        });
     }
 }
