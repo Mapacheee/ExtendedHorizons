3da96f6c4ff4bb1f11520bde3adbb651d5a6c2e3
Benjamín Isasmendi C. <bisasmendic@gmail.com>
2026-04-08 11:26:09 -0400

    fix: command injector

--- src/main/java/me/mapacheee/extendedhorizons/commands/EhCommands.java
+++ src/main/java/me/mapacheee/extendedhorizons/commands/EhCommands.java
@@ -2,6 +2,7 @@ package me.mapacheee.extendedhorizons.commands;
 
 import org.incendo.cloud.annotations.Command;
 
+import com.google.inject.Inject;
 import com.thewinterframework.command.CommandComponent;
 import com.thewinterframework.service.ReloadServiceManager;
 
@@ -10,6 +11,7 @@ public class EhCommands {
 
     private final ReloadServiceManager reloadServiceManager;
 
+    @Inject
     public EhCommands(ReloadServiceManager reloadServiceManager) {
         this.reloadServiceManager = reloadServiceManager;
     }
