a8bcc2eb0071d6e7f72bf7066da038c10fb04301
Benjamín Isasmendi C. <bisasmendic@gmail.com>
2026-04-08 10:55:51 -0400

    feat: add reload command

---
+++ src/main/java/me/mapacheee/extendedhorizons/commands/EhCommands.java
@@ -0,0 +1,22 @@
+package me.mapacheee.extendedhorizons.commands;
+
+import org.incendo.cloud.annotations.Command;
+
+import com.thewinterframework.command.CommandComponent;
+import com.thewinterframework.service.ReloadServiceManager;
+
+@CommandComponent
+public class EhCommands {
+
+    private final ReloadServiceManager reloadServiceManager;
+
+    public EhCommands(ReloadServiceManager reloadServiceManager) {
+        this.reloadServiceManager = reloadServiceManager;
+    }
+
+    @Command("eh reload")
+    public void reloadCommand() {
+        reloadServiceManager.reload();
+    }
+    
+}
